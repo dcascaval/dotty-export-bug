163a6fcdfe219afaaec7274128a9b0efa7b8c91b
Dan Cascaval <dan@cascaval.us>
2021-12-21 11:25:24 -0800

    add cases

--- src/main/scala/foo/main.scala
+++ src/main/scala/foo/main.scala
@@ -1,16 +1,20 @@
-package foo
-
 object api:
   export impl.*
 
 object impl:
   class Bar
-  extension (rcv: Bar) def bar(a: Int, b: Boolean = false) = ()
+  extension (rcv: Bar)
+    def foo(a: Int) = ()
+    def bar(a: Int, b: Boolean = false) = ()
 
 object Test1:
   import api.*
-  val value = (new Bar).bar(0)
+  val value = (new Bar).bar(0) // Missing argument
 
 object Test2:
   import impl.*
-  val value = (new Bar).bar(0)
+  val value = (new Bar).bar(0) // Works
+
+object Test3:
+  import api.*
+  val value = (new Bar).foo(0) // Works
